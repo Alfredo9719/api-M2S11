c81b7d0957b4e6d14e24a718c00da32f8af27360
Alfredo GM <alfredogonzalezm265@gmail.com>
2023-10-21 12:20:45 -0600

    corte total

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,30 @@
 console.log("Entro index.js");
+
+let contenidoProducto = document.getElementById("contenidoProductos");
+
+fetch('https://dev4humans.com.mx/api/clases/productos', {method: "GET"})
+    .then(response => response.json())
+    .then(data => {
+        console.log("La promesa se resolvio");
+        const rawData = data;
+        const productos = rawData.data.products;
+        console.log(productos);
+        productos.forEach(producto => {
+            contenidoProducto.innerHTML += ` <div class="col-md-12">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="${producto.thumbnail}" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${producto.title} - $${producto.price}</h5>
+                      <p class="card-text">${producto.description}</p>
+                      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>`;
+        });
+    }).catch(error => console.log(error))
